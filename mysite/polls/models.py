639c76b8d7326a86f58b09ce599a366408189dcf
NikoCSUMB <70172259+NikoCSUMB@users.noreply.github.com>
2021-09-29 17:21:28 -0700

    Database Schema

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    isAdmin = models.BooleanField(default=False)
+
+class Item(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+
+class Wishlist(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
+
