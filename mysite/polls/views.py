b8df29ed310dfdc27894a43c8d22955e10f79d1c
NikoCSUMB <70172259+NikoCSUMB@users.noreply.github.com>
2021-10-05 13:09:25 -0700

    Insert User into Database
    Creates a new user using inputs from the form and inserts them into the User table via POST.
    
    No validation yet.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.template import loader
+from .models import User
 # Create your views here.
 
 
@@ -9,4 +10,14 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def create(request):
+    if request.method=="POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        newUser = User()
+        newUser.username = username
+        newUser.password = password
+        newUser.save()
+
+        return redirect("/polls/create")
     return render(request, 'create.html')
