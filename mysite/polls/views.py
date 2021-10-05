b5509d4707b6691d05d04090c1b461f2867377fa
NikoCSUMB <70172259+NikoCSUMB@users.noreply.github.com>
2021-10-05 12:14:53 -0700

    Creation View

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def create(request):
+    return render(request, 'create.html')
