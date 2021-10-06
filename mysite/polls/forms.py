ec4d1e46e1b70e348cf1ede2c8079ca3b43d30bf
NikoCSUMB <70172259+NikoCSUMB@users.noreply.github.com>
2021-10-06 12:18:14 -0700

    Password Validation
    Creation now checks if the password is >=6 characters and alphanumeric

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -3,8 +3,21 @@ from .models import User
 from django import forms
 
 class CreationForm(forms.Form):
-    model = User
-    #fields = ('username', 'password')
+    class Meta:
+        model = User
+
     username = forms.CharField(label='Username', max_length=20)
-    # TODO: Make into password input
     password = forms.CharField(widget=forms.PasswordInput, label='Password', max_length=20)
+    def clean(self):
+        super(CreationForm, self).clean()
+        
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if len(password) < 6:
+            self._errors['password'] = self.error_class(['Password must be at least 6 characters long!'])
+
+            return self.cleaned_data
+        if not password.isalnum():
+            self._errors['password'] = self.error_class(['Password must be alphanumeric! (A-Z, 0-9)'])
+
