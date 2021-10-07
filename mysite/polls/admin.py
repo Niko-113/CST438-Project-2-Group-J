e6756a02126e830f569f70a22f9633dd81c8b996
amemariee <awall@csumb.edu>
2021-10-06 20:15:00 -0700

    creating products for wishlist

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from .models import User, Item, Wishlist
+from .models import User, Item, Wishlist, Product, Category
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Item)
 admin.site.register(Wishlist)
+
+@admin.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'slug', 'price',
+                    'in_stock', 'created', 'updated']
+    list_filter = ['in_stock', 'is_active']
+    list_editable = ['price', 'in_stock']
+    prepopulated_fields = {'slug': ('title',)}
