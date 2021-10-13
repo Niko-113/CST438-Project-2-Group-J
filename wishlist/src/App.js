6e302528d6855ea9f852a9cc74315869417e89ef
ctm93 <crismartinez@csumb.edu>
2021-10-13 11:32:02 -0700

    Added Login as landing page

--- wishlist/src/App.js
+++ wishlist/src/App.js
@@ -4,6 +4,7 @@ import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import Homepage from "./Pages/Homepage";
 import AdminLogin from "./AdminPages/AdminLogin";
+import AdminHomepage from "./AdminPages/AdminHomepage";
 
 //Landing page. User required to register and login to get access to Homepage
 function App() {
@@ -11,15 +12,16 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
+          <Route path="/" exact component={Login} /> 
           <Route path="/Login" exact component={Login} />
           <Route path="/Register" exact component={Register} />
           <Route path="/Homepage" exact component={Homepage} />
           <Route path="/AdminLogin" exact component={AdminLogin} />
+          <Route path="/AdminHomepage" exact component={AdminHomepage} />
         </Switch>
       </Router>
     </div>
 
-    
   );
 }
 
