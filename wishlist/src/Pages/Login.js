1a9b88f52208498be4ff1b7062dc084e71c85f04
Niko <70172259+NikoCSUMB@users.noreply.github.com>
2021-10-18 17:39:52 -0700

    Login API
    Form validation and submission for logging in via REST...
    
    No actual middleware set up yet, meaning the user isn't actually logged in

--- wishlist/src/Pages/Login.js
+++ wishlist/src/Pages/Login.js
@@ -1,18 +1,43 @@
 import React, { useState } from "react";
+import axios from "axios";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "../styles/Login.css";
 
 export default function Login() {
-  const [username, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+
+  const [formValues, setFormValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  function handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name
+    setFormValues({
+      ...formValues,
+      [name]: value
+    });
+  }
 
   function validateForm() {
-    return username.length > 0 && password.length > 0;
+    return formValues.username.length > 0 && formValues.password.length > 0;
   }
 
   function handleSubmit(event) {
     event.preventDefault();
+    axios.post("http://localhost:8000/polls/login/", formValues).catch(function (error) {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        document.querySelector("#loginError").innerHTML = "Invalid username or password";
+        document.querySelector("#loginError").style.color = "red"
+      }
+    });
+    document.querySelector("#loginError").innerHTML = "Login successful!";
+    document.querySelector("#loginError").style.color = "green";
   }
 
   return (
@@ -23,16 +48,19 @@ export default function Login() {
           <Form.Control
             autoFocus
             type="username"
-            value={username}
-            onChange={(e) => setUserName(e.target.value)}
+            name="username"
+            value={formValues.username}
+            onChange={handleInputChange}
           />
         </Form.Group>
+        <div id="loginError"></div>
         <Form.Group size="lg" controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            value={formValues.password}
+            onChange={handleInputChange}
           />
         </Form.Group>
         <Button block size="lg" type="submit" disabled={!validateForm()}>
